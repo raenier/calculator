72f3ad7aaa3ae0c4f321c53757b2d48132c2c889
raenier <raenierquiambao@gmail.com>
2023-05-03 15:54:17 +0800

    Catch undefined operators and dividing by zero

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+  if(num2 === 0) {return undefined};
+
   return num1 / num2;
 }
 
 function operate(num1, num2, operator) {
+  if(!['add', 'subtract', 'multiply', 'divide'].includes(operator)) { return undefined };
+
   return window[operator](num1, num2);
 }
