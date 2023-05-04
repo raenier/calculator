a29d78068964eb77a416894f7790dcf2cbc0b968
raenier <raenierquiambao@gmail.com>
2023-05-04 15:23:37 +0800

    Get number input and add/concatinate it to displayed number

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ function operate(num1, num2, operator) {
 
   return window[operator](num1, num2);
 }
+
+const number_buttons = document.querySelectorAll('.btn.number');
+const display = document.querySelector('.display');
+let current_display_string = '';
+
+number_buttons.forEach(btn => btn.addEventListener('click', (e) => {
+  //get value of button
+  input = e.target.innerHTML;
+  //get current displayed string on div
+  current_display_string = (display.innerHTML == 0) ? '' : display.innerHTML;
+  //concat button value to main string
+  current_display_string += input;
+  //update div display
+  display.textContent = current_display_string;
+} ));
