af0177d17852ea3dd226b47598431b6806140cd3
raenier <raenierquiambao@gmail.com>
2023-05-05 14:05:23 +0800

    Catch division by zero

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  if(num2 === 0) {return undefined};
+  if(num2 === 0) {return 'YOU P***E OF SH*T'};
 
   return num1 / num2;
 }
